ba313d9b4d242d94139b5420a4ffba21dbd6ac5c
tomlee19 <142337276+tomlee19@users.noreply.github.com>
2025-05-20 15:10:54 +0700

    update backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
-const IMAGE_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://letonblogenterprise.site/api';
+const IMAGE_BASE_URL = 'https://letonblogenterprise.site';
 
 
 export {API_BASE_URL, IMAGE_BASE_URL};
